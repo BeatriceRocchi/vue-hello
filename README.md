59248827440f2089635ae46ba70f74b980b7cb32
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-11 15:12:28 +0100

    [add] clock

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Vue Hello
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+## Svolgimento
+1. Esercizio base: 
+    - destrutturare l'oggetto Vue per recuperare il metodo createApp
+    - innestare con "mount" il metodo createApp nel div in html con id="app"
+    - in createApp, fornire come return del metodo data() il messaggio da mostrare in pagina
+
+2. Esercizio bonus:
+    - aggiungere nel return del metodo data() anche image con l'url dell'immagine da mostrare in pagina
+    - in html, inserire image nel src del tag img
+
+3. Reattività aggiunte:
+    - Orologio
--- js/script.js
+++ js/script.js
@@ -3,8 +3,31 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue",
+      message: "Hello, welcome in",
       image: "./assets/img/img1.png",
+      time: "00:00:00",
+      textColor: "green",
+      textBold: "bold",
     };
+  },
+
+  methods: {
+    printTime() {
+      const date = new Date();
+      let h = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+      let m =
+        date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+      let s =
+        date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+      this.time = `${h}:${m}:${s}`;
+    },
+    updateTime() {
+      setInterval(() => this.printTime(), 1000);
+    },
+  },
+
+  mounted() {
+    this.printTime();
+    this.updateTime();
   },
 }).mount("#app");
